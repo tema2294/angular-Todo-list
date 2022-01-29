814a563439d08e11b4d1a51c6c8785fbc01c9022
tema2294 <tema.marinin@mail.ru>
2022-01-29 22:46:37 +0300

    TodoList Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+export class Todo {
+  name: string;
+  isCompleted: boolean;
+  id: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-testapp';
+
+  todos: Todo[] = [];
+  newTodo: string;
+
+  saveTodo () {
+    if (this.newTodo) {
+      let todo = new Todo()
+      todo.isCompleted = true
+      todo.id = `${new Date()}`
+      todo.name = this.newTodo
+      this.todos.push(todo)
+      this.newTodo = ''
+    } else {
+      alert('please enter todo')
+    }
+  }
+  deleteItem (deletedId: string) {
+      this.todos = this.todos.filter(({id})=> id !== deletedId)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
